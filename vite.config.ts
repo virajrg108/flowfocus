4a34504273ae335b45b66682c27b566b1029c098
virajrg108 <viraj.gawde123@gmail.com>
2025-12-06 21:04:04 +0530

    Fix: Update base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
         theme_color: '#09090b',
         background_color: '#09090b',
         display: 'standalone',
-        start_url: '/',
+        start_url: '/flowfocus/',
         icons: [
           {
             src: 'logo.png',
@@ -30,5 +30,5 @@ export default defineConfig({
       }
     })
   ],
-  base: '/',
+  base: '/flowfocus/',
 })
